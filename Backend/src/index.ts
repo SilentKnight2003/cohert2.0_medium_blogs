a60ac1208bffc0dbc9e82d8b04a68f2d799c9af6
Lucifer <subhransujena2468@gmail.com>
2026-01-26 22:19:19 +0530

    feat: (login and blog routes)

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,62 +2,16 @@ import { Hono } from 'hono'
 import {PrismaClient} from "@prisma/client/edge"
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { schema } from './validation/auth'
-type Bindings = {
-  ACCELERATE_URL: string,
-}
-const app = new Hono<{Bindings:Bindings}>()
+import {decode,jwt,sign,verify} from "hono/jwt"
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
-app.post("/api/v1/user/signup",async(c) => {
-  // data got from body...
-  const data = await c.req.json();
-  // parse the data..
-  const parsed = schema.safeParse(data);
-  if(!parsed.success){
-    return c.text("Invalid data",401)
-  }
-  // connect to the prismaclient 
-  try{
-     const prisma =  new PrismaClient({
-     accelerateUrl:c.env.ACCELERATE_URL,
-     }).$extends(withAccelerate());
+const app = new Hono()
 
-    // found the user if it present..
-    const user = await prisma.user.findFirst({where:{
-      email:data.email
-    }})
-    if(user){
-       return c.text("User is present",401)
-    }
+app.route("/api/v1/user",userRouter)
+app.route("/api/v1/blog",blogRouter)
 
-    // create the user.
-    await prisma.user.create({
-     data:{
-       username:data.username,
-       email:data.email,
-       password:data.password
-     }
-    })
-  return c.text("successfully signin.")
 
-  }catch(Err){
-    return c.text("Internal server Error",500)
-  }
-  
-})
-app.post("/api/v1/user/signin",(c) => {
-  return c.text("signin")
-})
-app.post("/api/v1/blog",(c) => {
-  return c.text("signin")
-})
-app.put("/api/v1/blog",(c) => {
-  return c.text("signin")
-})
-app.get("/api/v1/blog/:id",(c) => {
-  return c.text("signin")
-})
-app.get("/api/v1/blog/bulk",(c) => {
-  return c.text("signin")
-})
+
 export default app
 
